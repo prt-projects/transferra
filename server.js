087d0e805a5cee984dd5e57f1f1012532865bc8a
Pranjal Thakur <thakurpranjal2003@gmail.com>
2026-05-09 17:16:18 +0530

    Initial commit for deployment

--- server.js
+++ server.js
@@ -17,13 +17,7 @@ const corsOptions = {
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
+
 
 app.use(cors(corsOptions))
 app.use(express.static('public'));
