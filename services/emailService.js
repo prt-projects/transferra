061e1648191789af54a951cfe1c0b78c1a734ec2
prt-projects <prthakur2003@gmail.com>
2026-05-19 12:10:31 +0530

    Fixed Brevo sender validation

--- services/emailService.js
+++ services/emailService.js
@@ -2,7 +2,8 @@ module.exports = async ({ from, to, subject, text, html }) => {
     const url = 'https://api.brevo.com/v3/smtp/email';
     
     const payload = {
-        sender: { email: from, name: "Transferra" },
+        sender: { email: process.env.MAIL_USER, name: "Transferra" },
+        replyTo: { email: from },
         to: [{ email: to }],
         subject: subject,
         textContent: text,
