a94fd4e3292755015f1c50db82cdab2feb099c5f
prt-projects <prthakur2003@gmail.com>
2026-05-19 11:24:01 +0530

    Cleaned up final email template branding

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -147,13 +147,7 @@ module.exports = ({emailFrom, downloadLink, size, expires}) => {
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Transferra Secure File Sharing</span>
-                                <br> Want to share a file? <a href="${process.env.APP_BASE_URL}" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Transferra</a>.
-                            </td>
-                            </tr>
-                            <tr>
-                            <td class="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                Powered by <a href="https://www.youtube.com/channel/UCo9xTRmg1SqQ5JSsA2fAgJw" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Pranjal & Partner</a>.
+                                Want to share a file? <a href="${process.env.APP_BASE_URL}" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Transferra</a>.
                             </td>
                             </tr>
                         </table>
